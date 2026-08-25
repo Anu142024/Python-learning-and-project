a98616f0ead6036dd9b38ebedee8ddb8fc73a9d1
anu <A.Das3353@student.leedsbeckett.ac.uk>
2026-08-26 00:49:37 +0100

    First code written

---
+++ Exercises/hello.py
@@ -0,0 +1,7 @@
+print("Hello, World!")
+
+print('Hello, World!')      #Both single quote and double quote works
+
+print(420)      #Prints as integer type
+
+print("420")        #Prints as string type
