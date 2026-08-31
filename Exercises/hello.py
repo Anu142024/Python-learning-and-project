1ad22429cf99551a26090409857fd6c151ab1ad6
anu <A.Das3353@student.leedsbeckett.ac.uk>
2026-09-01 00:51:55 +0100

    Basic learning of python coding

--- Exercises/hello.py
+++ Exercises/hello.py
@@ -5,3 +5,7 @@ print('Hello, World!')      #Both single quote and double quote works
 print(420)      #Prints as integer type
 
 print("420")        #Prints as string type
+
+'''Triple single or double quote is used for multiline commenting.
+Even though it's not exactly commenting rule of python, when we try to run the code,
+interpreter take the lines as string which is not under any function so it just avoids the lines'''
---
+++ Exercises/variables.py
@@ -0,0 +1,13 @@
+name = "anusuya"
+number = 1234
+
+print(name)         #prints out the string that is kept inside the variable
+print(number)       #prints out the integer that is kept inside the variable
+print("name")       #prints name value because it is mentioned as string not variable
+
+x = 25
+x = 50
+
+print(x)
+'''Python interpreter only takes the last value that is assigned to the variable
+So that means only one value can be assigned at a time .'''
