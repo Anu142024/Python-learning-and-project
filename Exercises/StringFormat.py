0004f196f77c9ebf54abf3bde417c127bf6c0f9c
anu <A.Das3353@student.leedsbeckett.ac.uk>
2026-09-04 11:26:50 +0100

    Different ways to format a string

---
+++ Exercises/StringFormat.py
@@ -0,0 +1,11 @@
+num1 = 20
+num2 = 10
+
+print("This is my super number {num1 + num2}")
+
+print(f"This is my super number {num1 + num2}")         # without f, python prints out the string inside quote as it is
+
+
+x = 30
+y = 50
+print("This is my super number", x + y)             # No need of f as the variables are outside the quote
