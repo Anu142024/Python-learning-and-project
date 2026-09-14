0e6a552d2951b0fda479509acd66659fffc9aa08
anu <A.Das3353@student.leedsbeckett.ac.uk>
2026-09-15 00:03:40 +0100

    Basic intro to various types of data type

--- Exercises/DataType.py
+++ Exercises/DataType.py
@@ -33,3 +33,50 @@ x = 8
 y = 10
 print(x > y)
 print(x < y)
+
+# Binary type data: bytes
+
+list1 = [1,2,3,121,255]              # bytes range is (0, 256), using any number over the range will give error
+
+b = bytes(list1)                # list of byte class can't be changed as it is immutable
+print(type(b))
+
+# Binary type data: byteArray
+
+list2 = [1,2,3,121,255]
+
+b1 = bytearray(list2)
+
+b1[1] = 100             # Only byteArray is mutable
+print(b1[1])            # We have changed index 1 or the 2nd element of the list
+
+# None type data
+
+x = None
+print(type(x))
+
+# list type data
+
+li = ["Anu","Sam","Nav","Shuv"]
+
+print(li)
+print(type(li))
+
+
+li1 = ["Anu","Sam","Nav"]           # list is mutable
+
+li1[0] = "Anusuya"
+print(li1)
+
+# tuple type data
+
+tup = (5,10,15,20,25)
+print(type(tup))            # tuple is immutable
+
+# range type data
+
+r = range(6)
+
+for i in r:
+    print(i)            # if r is written instead of i, it will only print range(0,6) 6 times
+
