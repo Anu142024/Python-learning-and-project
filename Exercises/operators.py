61069f7ab73bb8c482a3c842e2f4c1dd430918b3
anu <A.Das3353@student.leedsbeckett.ac.uk>
2026-09-22 13:34:23 +0100

    Arithmetic operators

---
+++ Exercises/operators.py
@@ -0,0 +1,28 @@
+# Arithmetic operator: Addition
+
+a = 10
+b = 20
+print(a + b)
+
+# Subtraction
+print(b - a)
+
+# Multiplication
+print(a * b)
+
+# Division
+print(b / a)
+
+# Modulus
+print(b % a)            # The remainder of 2 numbers
+
+# Exponentiation
+x = 5
+y = 3
+print(x ** y)           # 5 to the power of 3
+
+# Floor division
+sum1 = 15
+sum2 = 2
+print(sum1 // sum2)     # Rounds up the number output without the point values
+
