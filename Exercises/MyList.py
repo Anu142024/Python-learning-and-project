bcc15d7ec4d3eb581cf6b48a31c80c91c3ff2eaa
anu <A.Das3353@student.leedsbeckett.ac.uk>
2026-09-26 12:17:42 +0100

    More details about list

---
+++ Exercises/MyList.py
@@ -0,0 +1,14 @@
+list1 = ["apple","banana","cherry"]
+print(list1)
+
+list2 = [1,3,5,7,9]
+print(list2)
+
+list3 = [True, False, True]
+print(list3)
+
+list4 = ["apple", 2, False, "cherry", 5]
+print(list4)
+
+list4[0] = 100
+print(list4)
